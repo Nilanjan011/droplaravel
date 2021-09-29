e3d3984303b6a35961d935e0deabf219cd46ca84
Nilanjan011 <nilanjanchakraborty.nc@gmail.com>
2021-09-29 19:40:43 +0530

    fast2sms add

--- app/helper/function.php
+++ app/helper/function.php
@@ -8,4 +8,40 @@ function oneField($id,$field,$table){
     $item= DB::table($table)->select($field)->where('id',$id)->get();
     return $item[0]->$field;
 }
+
+function sms($phone,$otp)
+{
+    $fields = array(
+        "variables_values" => "$otp",
+        "route" => "otp",
+        "numbers" => "$phone",
+    );
+    
+    $curl = curl_init();
+    
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://www.fast2sms.com/dev/bulkV2",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_SSL_VERIFYHOST => 0,
+      CURLOPT_SSL_VERIFYPEER => 0,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => json_encode($fields),
+      CURLOPT_HTTPHEADER => array(
+        "authorization: 5wMTIWYPFv3aXl0ibKmd98CJpeHANVojgBshxOuUyGc1Z24S6zNjOb0EDdaqYmHpXCePRlLW56x3IwAF",
+        "accept: */*",
+        "cache-control: no-cache",
+        "content-type: application/json"
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    
+    curl_close($curl);
+    return $err;
+}
 ?>
