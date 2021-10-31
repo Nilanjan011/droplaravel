a93e51d8fea405cefc7e96be8a60c4d35ced0cc9
Madhusudan Jana <madhusudan1996.jana@gmail.com>
2021-10-31 10:59:43 +0530

    cvsdf

--- app/Http/Controllers/addController.php
+++ app/Http/Controllers/addController.php
@@ -24,4 +24,9 @@ class addController extends Controller
         // return DB::table('carti')->where("id",1)->count(); # pratice count method
         return DB::table('carti')->count();
     }
+    public function cvgdcfg()
+    {
+        // return DB::table('carti')->where("id",1)->count(); # pratice count method
+        return DB::table('carti')->count();
+    }
 }
